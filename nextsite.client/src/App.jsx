3266d951cbe9dcb493ae79e530919a1c49d62cec
mmartin46 <mmartin46@angelo.edu>
2024-07-10 12:21:37 -0500

    lets do it

--- nextsite.client/src/App.jsx
+++ nextsite.client/src/App.jsx
@@ -6,10 +6,28 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import PageCarousel from './components/general/PageCarousel';
 import Footer from './components/general/Footer';
 
+import './Icon.scss';
+
 const MainLayout = () => {
+
     return (
         <>
             <PageCarousel />
+            <div id="choices" className="row justify-content-center align-items-center">
+                <div className="col icon tint" style={{ background: "url('/public/metal_0.png')"}}>
+                    <h3>Weights</h3>
+                    <h6>Starting at $10.00</h6>
+                </div>
+                <div className="col icon tint" style={{ background: "url('/public/yoga.jpg')" }}>
+                    <h3>Bootcamps</h3>
+                    <h6>Starting at $30.00</h6>
+                </div>
+                <div className="col icon tint">
+                    <h3>Weights</h3>
+                    <h6>Starting at $10.00</h6>
+                </div>
+            </div>
+
         </>
     );
 }
