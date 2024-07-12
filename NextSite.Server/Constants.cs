5440fd295e845e0b4d0fdb4a1e4ea05ae29b49f0
mmartin46 <mmartin46@angelo.edu>
2024-07-12 12:15:03 -0500

    changes made

--- NextSite.Server/Constants.cs
+++ NextSite.Server/Constants.cs
@@ -1,9 +1,1 @@
-﻿namespace NextSite.Server
-{
-    public class Constants
-    {
-        public static string ConnectionString = "mongodb+srv://mitchellbmartin00:Isaiah4031";
-        public static string DatabaseName = "weight_website";
-        public static string CollectionName = "personal_trainers";
-    }
-}
+﻿
