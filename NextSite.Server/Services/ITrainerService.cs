94846fe624fc3e48a79a57ef1a853356513a97a5
mmartin46 <mmartin46@angelo.edu>
2024-07-12 12:11:05 -0500

    changes made

---
+++ NextSite.Server/Services/ITrainerService.cs
@@ -0,0 +1,13 @@
+﻿using NextSite.Server.Models;
+
+namespace NextSite.Server.Services
+{
+    public interface ITrainerService
+    {
+        Task CreateAsync(TrainerModel trainer);
+        Task<List<TrainerModel>> GetAsync();
+        Task<TrainerModel?> GetAsync(int id);
+        Task RemoveAsync(int id);
+        Task UpdateAsync(int id, TrainerModel updatedTrainer);
+    }
+}
---
+++ NextSite.Server/Services/TrainerService.cs
@@ -0,0 +1,33 @@
+﻿// John 3:5
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using NextSite.Server.Models;
+
+namespace NextSite.Server.Services
+{
+    public class TrainerService : ITrainerService
+    {
+        private readonly IMongoCollection<TrainerModel> _trainerCollection;
+        public TrainerService()
+        {
+            var mongoClient = new MongoClient("mongodb+srv://mitchellbmartin00:Isaiah4031");
+            var mongoDatabase = mongoClient.GetDatabase("weight_website");
+            _trainerCollection = mongoDatabase.GetCollection<TrainerModel>("personal_trainers");
+        }
+
+        public async Task<List<TrainerModel>> GetAsync() =>
+            await _trainerCollection.Find(_ => true).ToListAsync();
+
+        public async Task<TrainerModel?> GetAsync(int id) =>
+            await _trainerCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(TrainerModel trainer) =>
+            await _trainerCollection.InsertOneAsync(trainer);
+
+        public async Task UpdateAsync(int id, TrainerModel updatedTrainer) =>
+            await _trainerCollection.ReplaceOneAsync(x => x.Id == id, updatedTrainer);
+
+        public async Task RemoveAsync(int id) =>
+            await _trainerCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
