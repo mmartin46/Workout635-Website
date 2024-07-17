18320deb7b4a7db05570eb2229defb9e67025c17
mmartin46 <mmartin46@angelo.edu>
2024-07-17 08:53:25 -0500

    changes made

--- nextsite.client/src/App.jsx
+++ nextsite.client/src/App.jsx
@@ -9,67 +9,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Header from './components/general/Header';
 import MainLayout from './components/layouts/MainLayout';
-import Topic from './components/general/Topic';
-import './MemberCard.scss';
 import PersonalTrainingLayout from './components/layouts/PersonalTrainingLayout';
+import MembershipLayout from './components/layouts/MembershipLayout';
+
 
 const ScreenRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<MainLayout />} />
             <Route path="/training" element={<PersonalTrainingLayout />} />
-            <Route path="/memberships" element={<MembershipLayout/> }/>
+            <Route path="/memberships" element={<MembershipLayout />} />
         </Routes>
     );
-}
-
-const MembershipLayout = () => {
-    const [memberships, setMemberships] = useState(null);
-
-    useEffect(() => {
-        const getMemberTypes = () => {
-            $.ajax({
-                url: "https://localhost:44314/Memberships",
-                type: 'GET',
-                crossDomain: true,
-                dataType: 'json',
-                success: function (res) {
-                    setMemberships(res);
-                    console.log('Memberships-> ', memberships);
-                }
-            });
-        };
-
-        getMemberTypes();
-    }, [memberships]);
-
-
-    return (
-        <div>
-            <Topic
-                title="Membership Information"
-                caption="They improved dramatically once the lead singer left." />
-                <div className="member-container row">
-                    {memberships &&
-                        memberships.map((membership) => (
-                            <div className="col member-card" key={membership.id}>
-                                <h3>{membership.type}</h3>
-                                <div>
-                                    <h5><b>${membership.price.toFixed(2)}</b> / per month</h5>
-                                    <div>
-                                        <h5>Plan Includes:</h5>
-                                        <h6>Joiners Fee: ${membership.joinerFee.toFixed(2)}</h6>
-                                        <h6>Allow Guest: {membership.allowGuest ? "Yes" : "No"}</h6>
-                                    </div>
-                                    <div className="click-more">
-                                        <h4>Contact Us</h4>
-                                    </div>
-                                </div>
-                             </div>
-                    ))}
-                </div>
-        </div>
-    )
 }
 
 
