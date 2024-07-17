2956f5b677ff072dcbcaa4140a5f659b97a641aa
mmartin46 <mmartin46@angelo.edu>
2024-07-17 11:48:11 -0500

    changes added

--- nextsite.client/src/components/layouts/BootcampLayout.jsx
+++ nextsite.client/src/components/layouts/BootcampLayout.jsx
@@ -30,7 +30,14 @@ const BootCampLayout = () => {
                 caption="The fence was confused about whether it was supposed to keep things in or keep things out." />
 
 
-            <div>
+            <div className="main-padding">
+                <div className="heading-part row">
+                    <img src="/public/athlete.png" />
+                    <div className="topic col">
+                        <h1>What Do Our Bootcamps Provide?</h1>
+                        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+                    </div>
+                </div>
                 <div className="row">
                     {bootCamps && bootCamps.map((bootcamp) => (
                         <div className="col serv-div" key={bootcamp.id}>
