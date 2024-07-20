026571122d67507064c3fa8253659f188feba263
mmartin46 <mmartin46@angelo.edu>
2024-07-20 09:35:23 -0500

    changes made

--- nextsite.client/src/components/buttons/PlainLink.jsx
+++ nextsite.client/src/components/buttons/PlainLink.jsx
@@ -5,9 +5,12 @@ const PlainLink = (props) => {
 
     const { to , fontColor, children, className } = props;
 
+    const styledDelay = () => {
+        setTimeout(() => { }, 300);
+    };
 
     return (
-        <Link to={to} style={{ textDecoration: 'none', color: fontColor }}>
+        <Link to={to} style={{ textDecoration: 'none', color: fontColor }} onClick={styledDelay}>
             <span className={className}>
                 {children}
             </span>
